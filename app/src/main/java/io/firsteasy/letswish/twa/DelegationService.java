d13a7ad6ea8d1084641f9c516f77cec5821c71a8
mohamedadel80080 <mohamedadel80080@gmail.com>
2023-01-21 16:57:48 +0200

    first commit

---
+++ app/src/main/java/io/firsteasy/letswish/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.firsteasy.letswish.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
